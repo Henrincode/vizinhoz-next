109859c017f4b8c40061f45d5097dc5285e87e08
Henrincode <spellctro@gmail.com>
2025-12-21 16:45:24 -0300

    build: instalando lib postgres e criando conexão com banco de dados

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,13 @@
-export default function Home() {
-    return (
-        <>
-            <h1>Olá Mundo!</h1>
-        </>
-    )
+import sql from '@/lib/db';
+
+export default async function Home() {
+
+    const users = await sql`
+        select u.nome nomeeee, c.nome condominio, u.bloco, u.apartamento
+        from vz_tb_usuarios u
+        inner join vz_tb_condominios c
+            on c.id_condominio = u.id_condominio_fk
+    `
+
+    return <pre>{JSON.stringify(users, null, 2)}</pre>;
 }
