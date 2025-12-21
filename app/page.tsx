35cca838824138fa42fdc0394fe3c18b369a4d6d
Henrincode <spellctro@gmail.com>
2025-12-21 17:40:56 -0300

    feat: removendo dynamic da home

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,4 @@
-export const dynamic = 'force-dynamic';
-
-import sql from '@/lib/db';
-
 export default async function Home() {
 
-    const users = await sql`
-        select u.nome nomeeee, c.nome condominio, u.bloco, u.apartamento
-        from vz_tb_usuarios u
-        inner join vz_tb_condominios c
-            on c.id_condominio = u.id_condominio_fk
-    `
-
-    return <pre>{JSON.stringify(users, null, 2)}</pre>;
+    return <pre>Olá Mundo</pre>;
 }
